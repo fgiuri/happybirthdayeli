8f890ff4e40be132c73a6932ca3fa73e895c4e7f
Francesco Giuri <francescog.management@gmail.com>
2026-09-05 02:21:59 +0200

    Update START_DATE to new time

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ function loop(t){
 requestAnimationFrame(loop);
 
 // ================= LIVE COUNTER =================
-const START_DATE = new Date('2021-01-12T00:00:00');
+const START_DATE = new Date('2021-01-12T18:00:00');
 
 function updateCounter(){
   const now = new Date();
